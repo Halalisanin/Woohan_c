15799db279a03bc695c4785fa471395393ccc657
Halalisanin <halalisanin@gmail.com>
2023-06-17 15:41:15 -0400

    first c program

---
+++ hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 (Success)
+ */
+
+ int main(void)
+{
+	/* Print the message to the console */
+	printf("Hello, world!\n");
+
+	return 0;
+}
